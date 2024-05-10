6c526b034845b23c791ec2a5d905b7b552732179
Fazlul Alam <fazlulalam619@gmail.com>
2024-05-10 21:18:20 +0600

    NavBar And Banner Section

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+  const navLink = (
+    <>
+      <li>
+        <Link className="font-bold" to="/home">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="font-bold" to="/availableFoods">
+          Available Foods
+        </Link>
+      </li>
+      <li>
+        <Link className="font-bold" to="/addFood">
+          Add Food
+        </Link>
+      </li>
+      <li>
+        <Link className="font-bold" to="/manageMyFoods">
+          Manage My Foods
+        </Link>
+      </li>
+      <li>
+        <Link className="font-bold" to="/myFoodRequest">
+          My Food Request
+        </Link>
+      </li>
+    </>
+  );
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 h-24 mb-4">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -24,52 +55,16 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {navLink}
             </ul>
           </div>
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navLink}</ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <button className="btn btn-outline btn-success">LOG IN</button>
         </div>
       </div>
     </div>
